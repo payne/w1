40b063b01c53e9f967d145ba0e161a766365d1e3
Matt Payne <Payne@MattPayne.org>
2019-03-20 16:41:57 -0500

    BandMembersLookup forwards to members.jsp

---
+++ src/main/java/org/mattpayne/web/w1/BandMembersLookup.java
@@ -0,0 +1,61 @@
+package org.mattpayne.web.w1;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BandMembersLookup
+ */
+public class BandMembersLookup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BandMembersLookup() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// https://learning.oreilly.com/library/view/head-first-servlets/9780596516680/ch03s18.html
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		List<String> lstMembers = new ArrayList<>();
+		lstMembers.add("John");
+		lstMembers.add("Paul");
+		request.setAttribute("members", lstMembers);
+		RequestDispatcher view = request.getRequestDispatcher("members.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
