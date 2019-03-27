7778c3ba882cebedd2ebd57a6c13bb9889edaccf
Matt Payne <Payne@MattPayne.org>
2019-03-26 20:21:34 -0500

    broken

--- src/main/java/org/mattpayne/web/w1/Hello.java
+++ src/main/java/org/mattpayne/web/w1/Hello.java
@@ -3,8 +3,6 @@ package org.mattpayne.web.w1;
 import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
